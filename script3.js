15962850111938fe9dc241b921a9b9356a23512b
marrrrianaaaa <110055268+marrrrianaaa@users.noreply.github.com>
2022-07-27 19:01:40 +0400

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src=anything;
+}
+function changeCircleColor(coLor){
+    const circle = document.querySelector('.circle');
+    circle.style.background=coLor
+}
